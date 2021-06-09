31972f690d8b810bcc25be20c9c997f145712813
Saoud <saoud@saoud.dev>
2021-06-08 20:51:07 -0700

    does things and stuff, adds user login, logout, adds new models and controllers
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

--- MVCLibrary/Controllers/BooksControllers.cs
+++ MVCLibrary/Controllers/BooksControllers.cs
@@ -4,6 +4,10 @@
 // using MVCLibrary.Models;
 // using System.Collections.Generic;
 // using System.Linq;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Identity;
+// using System.Threading.Tasks;
+// using System.Security.Claims;
 
 // public ActionResult Index()
 // {
